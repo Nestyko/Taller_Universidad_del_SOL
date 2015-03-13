b2f58f9c7e758ed80cbbae334d60115c0883b526
Nestor Luis Tobon <nltobon@gmail.com>
2015-03-13 01:41:25 -0430

    Cambiar Scanner por KbInput
    en clases auxiliares de Nestor
    *hay un error en el vector notas

---
+++ Taller Universidad del SOL/src/aux_classes/nestor/input_output/KbInput/KbInput.java
@@ -0,0 +1,32 @@
+package aux_classes.nestor.input_output.KbInput;
+/**
+
+
+ * @author Mordamir & Michelangelo Partipilo
+ *
+ * Keyboard Read Class (for lectures)
+ */
+
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+//import java.io.*;
+
+public class KbInput {
+	private static String input;
+
+	// Read Method
+	public static String read() {
+		try {
+			Reader rin = new InputStreamReader(System.in);
+			BufferedReader brin = new BufferedReader(rin);
+			input = brin.readLine();
+			}
+		catch (IOException e) {
+			System.exit(1);
+			}
+			return input;
+  }
+}
